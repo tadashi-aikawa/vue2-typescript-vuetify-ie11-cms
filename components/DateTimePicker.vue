02ce6b8b9c2acde6701e9f61bbb0046269e3d9e8
tadashi-aikawa <syou.maman@gmail.com>
2021-02-04 00:44:31 +0900

    DatePicker, TimePicker系を追加

---
+++ components/DateTimePicker.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="d-flex">
+    <v-menu
+      v-model="state.dateMenu"
+      :close-on-content-click="false"
+      offset-y
+      min-width="auto"
+    >
+      <template #activator="{ on, attrs }">
+        <v-text-field
+          :value="state.displayDate"
+          label="日付"
+          prepend-icon="mdi-calendar"
+          readonly
+          v-bind="attrs"
+          style="width: 120px"
+          v-on="on"
+        ></v-text-field>
+      </template>
+      <v-date-picker
+        v-model="state.displayDate"
+        no-title
+        scrollable
+        color="cyan darken-1"
+        @change="changeDate"
+      />
+    </v-menu>
+    <v-menu
+      v-model="state.timeMenu"
+      :close-on-content-click="false"
+      offset-y
+      min-width="auto"
+    >
+      <template #activator="{ on, attrs }">
+        <v-text-field
+          :value="state.displayTime"
+          label="時間"
+          prepend-icon="mdi-clock"
+          readonly
+          v-bind="attrs"
+          class="ml-3"
+          style="width: 80px"
+          v-on="on"
+        ></v-text-field>
+      </template>
+      <v-time-picker
+        v-model="state.displayTime"
+        format="24hr"
+        color="cyan darken-1"
+        @change="changeTime"
+      ></v-time-picker>
+    </v-menu>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from '@vue/composition-api'
+import { DateTime } from 'owlelia'
+
+export default defineComponent({
+  props: {
+    dateTime: { type: Object as () => DateTime, default: null },
+  },
+  setup(_props, { emit }) {
+    const state = reactive({
+      displayDate: '',
+      displayTime: '',
+      dateMenu: false,
+      timeMenu: false,
+    })
+
+    const changeDate = (date: string) => {
+      emit('update:dateTime', DateTime.of(`${date} ${state.displayTime}`))
+    }
+    const changeTime = (time: string) => {
+      emit('update:dateTime', DateTime.of(`${state.displayDate} ${time}`))
+    }
+
+    return {
+      state,
+      changeDate,
+      changeTime,
+    }
+  },
+})
+</script>
--- pages/events/admin.vue
+++ pages/events/admin.vue
@@ -1,35 +1,54 @@
 <template>
   <v-row justify="center" align="center" style="height: 100%">
-    <v-col cols="12" md="4">
+    <v-col cols="12" xs="6" sm="8" md="10" lg="8" xl="6">
       <v-form ref="form" v-model="state.valid" lazy-validation>
-        <v-select
-          v-model="state.world"
-          :items="worlds"
-          label="世界"
-          return-object
-          item-text="name"
-        ></v-select>
-        <v-select
-          v-if="state.world"
-          v-model="state.goal"
-          :items="state.world.goals"
-          label="目的"
-          return-object
-          item-text="name"
-        ></v-select>
-        <v-row v-if="state.goal">
-          <v-col cols="12" md="6">
+        <v-row dense>
+          <v-col cols="12" md="2" class="d-flex align-center justify-center">
+            世界
+          </v-col>
+          <v-col cols="12" md="10" class="d-flex align-center justify-center">
             <v-select
-              v-model="state.section.begin"
+              v-model="state.world"
+              :items="worlds"
+              label="世界(必須)"
+              return-object
+              item-text="name"
+            ></v-select>
+          </v-col>
+        </v-row>
+        <v-row v-if="state.world" dense>
+          <v-col cols="12" md="2" class="d-flex align-center justify-center">
+            目的
+          </v-col>
+          <v-col cols="12" md="10" class="d-flex align-center justify-center">
+            <v-select
+              v-model="state.goal"
+              :items="state.world.goals"
+              label="目的(必須)"
+              return-object
+              item-text="name"
+            ></v-select>
+          </v-col>
+        </v-row>
+        <v-row v-if="state.goal" dense>
+          <v-col cols="12" md="2" class="d-flex align-center justify-center">
+            発生区間
+          </v-col>
+          <v-col cols="12" md="4">
+            <v-select
+              v-model="state.beginCity"
               :items="state.goal.cities"
               label="区間(開始)"
               return-object
               item-text="name"
             ></v-select>
           </v-col>
-          <v-col cols="12" md="6">
+          <v-col cols="12" md="1" class="d-flex align-center justify-center">
+            <span class="display-1">~</span>
+          </v-col>
+          <v-col cols="12" md="4">
             <v-select
-              v-model="state.section.end"
+              v-model="state.endCity"
               :items="state.goal.cities"
               label="区間(終了)"
               return-object
@@ -37,11 +56,25 @@
             ></v-select>
           </v-col>
         </v-row>
+        <v-row v-if="state.goal" dense>
+          <v-col cols="12" md="2" class="d-flex align-center justify-center">
+            発生期間
+          </v-col>
+          <v-col cols="12" md="4">
+            <date-time-picker :date-time.sync="state.beginDate" />
+          </v-col>
+          <v-col cols="12" md="1" class="d-flex align-center justify-center">
+            <span class="display-1">~</span>
+          </v-col>
+          <v-col cols="12" md="4">
+            <date-time-picker :date-time.sync="state.beginDate" />
+          </v-col>
+        </v-row>
 
         <div class="d-flex justify-center pa-6">
           <v-btn
             :disabled="!state.valid"
-            color="success"
+            color="success darken-2"
             class="mr-4"
             @click="submit"
           >
@@ -58,21 +91,26 @@
 
 <script lang="ts">
 import { computed, defineComponent, reactive, ref } from '@vue/composition-api'
+import { DateTime } from 'owlelia'
 import { World } from '~/domain/quests/entity/World'
 import { worldStore } from '~/utils/store-accessor'
 import { Goal } from '~/domain/quests/entity/Goal'
 import { City } from '~/domain/quests/entity/City'
-import { Section } from '~/domain/common/vo/Section'
+import { Nullable } from '~/utils/types'
+import DateTimePicker from '~/components/DateTimePicker.vue'
 
 export default defineComponent({
+  components: {
+    DateTimePicker,
+  },
   setup() {
     const state = reactive({
-      world: null as World | null,
-      goal: null as Goal | null,
-      section: {
-        begin: null,
-        end: null,
-      } as Section<City | null>,
+      world: null as Nullable<World>,
+      goal: null as Nullable<Goal>,
+      beginCity: null as Nullable<City>,
+      endCity: null as Nullable<City>,
+      beginDate: null as Nullable<DateTime>,
+      isBeginDateVisible: false,
       valid: false,
     })
 
